3a6f2b5fc63c7be6118443c2b96322797f053e48
Lorenzo La Mura <lorenzo.la-mura@alumni.univ-avignon.fr>
2023-03-08 18:42:11 +0100

    Add new example

--- pareto.py
+++ pareto.py
@@ -13,8 +13,9 @@ def pareto_optimal_options(data, directions):
 if __name__ == '__main__':
     data_example = pd.DataFrame({
         'house': ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
+        'C1': [80, 65, 83, 40, 52, 94],
         'C2': [90, 58, 60, 80, 72, 96],
         'C6': [5, 1, 7, 10, 8, 6],
     })
-    directions_example = {'C2': 'max', 'C6': 'max'}
+    directions_example = {'C1': 'min', 'C2': 'max', 'C6': 'max'}
     print(pareto_optimal_options(data_example, directions_example))

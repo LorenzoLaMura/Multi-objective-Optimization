05b2380c2c494407795750c06898f2d1f18d2072
Lorenzo La Mura <lorenzo.la-mura@alumni.univ-avignon.fr>
2023-03-11 17:26:36 +0100

    Fix weighted sum

--- weighted_sum.py
+++ weighted_sum.py
@@ -10,6 +10,19 @@ def weighted_sum(data, weights, directions):
     return weighted_sums
 
 
+def normalize_data(data):
+    cols_to_normalize = [col for col in data.columns[1:] if data[col].dtype != object]
+    for col in cols_to_normalize:
+        data[col] = (data[col] - data[col].min()) / (data[col].max() - data[col].min())
+    return data
+
+
+def get_ranking(data, weighted_sums):
+    tuples = list(zip(data.iloc[:, 0], weighted_sums))
+    sorted_tuples = sorted(tuples, key=lambda x: x[1], reverse=True)
+    return [t[0] for t in sorted_tuples]
+
+
 if __name__ == '__main__':
     data_example = pd.DataFrame({
         'house': ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
@@ -19,4 +32,7 @@ if __name__ == '__main__':
     })
     directions_example = {'C1': 'min', 'C2': 'max', 'C6': 'max'}
     weights_example = {'C1': 0.2, 'C2': 0.4, 'C6': 0.4}
-    print(weighted_sum(data_example, weights_example, directions_example))
+    normalized_data = normalize_data(data_example)
+    weighted_sum_example = weighted_sum(normalized_data, weights_example, directions_example)
+    print(weighted_sum_example)
+    print(get_ranking(data_example, weighted_sum_example))
